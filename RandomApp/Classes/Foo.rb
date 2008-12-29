90d3933d9881773c11d57cb843b69d29d1d4b091
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-29 15:08:07 +1000

    initial RandomApp import

---
+++ RandomApp/Classes/Foo.rb
@@ -0,0 +1,20 @@
+#
+#  Foo.rb
+#  RandomApp
+#
+#  Created by Dr Nic on 29/12/08.
+#  Copyright (c) 2008 Mocra. All rights reserved.
+#
+
+# see http://upstream-berlin.com/2008/10/28/cocoa-for-rubyists-with-macruby/
+class Foo
+	attr_accessor :textField
+
+	def seed(sender)
+		textField.StringValue = "Generator doesn't need to be seeded ;)"  
+	end
+
+	def generate(sender)
+		textField.StringValue = (rand(100) + 1)
+	end
+end
