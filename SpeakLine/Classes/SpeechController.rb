a8a78b9e6a3d453a40bfa155aab57c3cd184b7be
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-29 20:51:14 +1000

    table of voices, with default one initially selected

--- SpeakLine/Classes/SpeechController.rb
+++ SpeakLine/Classes/SpeechController.rb
@@ -8,22 +8,29 @@
 
 class SpeechController
 	attr_accessor :textField, :startButton, :stopButton
-	attr_accessor :speechSynth
+	attr_accessor :speechSynth, :voicesTableView
+	attr_accessor :voiceList
 	
 	def initialize
 		@speechSynth = NSSpeechSynthesizer.alloc.initWithVoice(nil)
+		@voiceList = NSSpeechSynthesizer.availableVoices
 		speechSynth.delegate = self
 	end
 	
 	def awakeFromNib
 		stopButton.enabled = false
+		defaultVoice = NSSpeechSynthesizer.defaultVoice
+		defaultRow = voiceList.indexOfObject(defaultVoice)
+		voicesTableView.selectRow(defaultRow, byExtendingSelection: false)
+		voicesTableView.scrollRowToVisible(defaultRow)
 	end
 
 	def sayIt(sender)
 		if textField.stringValue.size > 0
 			speechSynth.startSpeakingString(textField.stringValue)
-			stopButton.enabled = true
-			startButton.enabled = false
+			stopButton.enabled      = true
+			startButton.enabled     = false
+			voicesTableView.enabled = false
 		end
 	end
 
@@ -31,10 +38,28 @@ class SpeechController
 		speechSynth.stopSpeaking
 		stopButton.enabled = false
 		startButton.enabled = true
+		voicesTableView.enabled = true
 	end
 	
 	def speechSynthesizer(sender, didFinishSpeaking: success)
-		
+		stopButton.enabled = false
+		startButton.enabled = true
+		voicesTableView.enabled = true
 	end
+
+	def numberOfRowsInTableView(aTableView)
+		voiceList.count
+	end
+	
+	def tableView(aTableView, objectValueForTableColumn: aTableColumn, row: rowIndex)
+		voice = voiceList.objectAtIndex(rowIndex)
+		NSSpeechSynthesizer.attributesForVoice(voice).objectForKey:"VoiceName"
+	end
+	
+	def tableViewSelectionDidChange(aNotification)
+		return if voicesTableView.selectedRow == -1
+		speechSynth.voice = voiceList.objectAtIndex(voicesTableView.selectedRow)
+	end
+	
 	
 end
