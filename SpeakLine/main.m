086a46861180f5905a700817f9e4b0c8822b020e
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-29 15:20:27 +1000

    initial IB for SpeakLine

---
+++ SpeakLine/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SpeakLine
+//
+//  Created by Dr Nic on 29/12/08.
+//  Copyright Mocra 2008. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
