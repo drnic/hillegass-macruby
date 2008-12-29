98287f58612e59252a3fd2f991ead59120fbbf27
Dr Nic Williams <drnicwilliams@gmail.com>
2008-12-29 15:49:35 +1000

    Challenge from Target/Action chapter

---
+++ Challenges/Target-Action/Classes/CounterController.rb
@@ -0,0 +1,20 @@
+#
+#  CounterController.rb
+#  Target-Action
+#
+#  Created by Dr Nic on 29/12/08.
+#  Copyright (c) 2008 Mocra. All rights reserved.
+#
+
+class CounterController
+	attr_accessor :labelField, :textField
+
+	def awakenFromNib
+		labelField.stringValue = "???"
+		textField.stringValue  = ""
+	end
+	
+	def countCharacters(sender)
+		labelField.StringValue = "'#{textField.stringValue}' has #{textField.stringValue.size} characters"
+	end
+end
